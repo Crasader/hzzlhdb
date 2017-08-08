b7a1a2e63576c51e9c2453559265a89c0842c755
WangJunHui4815 <673131434@qq.com>
2017-08-08 10:33:02 +0800

    by wjh
    add lhdb dir

---
+++ hzzgame/Classes/games/lhdb/lhdb_special_effects.cpp
@@ -0,0 +1,11 @@
+#include "lhdb_special_effects.h"
+
+
+lhdb_special_effects::lhdb_special_effects()
+{
+}
+
+
+lhdb_special_effects::~lhdb_special_effects()
+{
+}
---
+++ hzzgame/Classes/games/lhdb/lhdb_special_effects.h
@@ -0,0 +1,8 @@
+#pragma once
+class lhdb_special_effects
+{
+public:
+	lhdb_special_effects();
+	~lhdb_special_effects();
+};
+
